560b7ed5d8da0043c58dc16a3cacac0aee34768a
tbasdev57 <100427467+tbasdev57@users.noreply.github.com>
2022-06-09 14:37:37 +0200

    Add files via upload

---
+++ Evaluation Js BASBUNAR Tayyib/eval.js
@@ -0,0 +1,15 @@
+$(document).ready(function () {
+  $('#carouselExampleSlidesOnly').carousel({
+    interval: 15000,
+    pause: false,
+  });
+
+  
+  $('#carouselExampleIndicators').carousel({
+    interval: 0,
+    pause: false,
+    cycle: 0,
+  });
+
+
+});
